34d7992c559a02d4d6b44b91dd92dce5aa52a87a
sarika lakhe <sarikalakhe1980@yahoo.co.uk>
2023-10-29 13:08:33 +0000

    instance

---
+++ instancevariable/InstanceVariable1.java
@@ -0,0 +1,16 @@
+package instancevariable;
+
+public class InstanceVariable1 {
+            int a=10;
+
+            void method1(){
+
+            }
+            int b=20;
+            void method2(){
+
+            }
+public static void main (String[]args){
+
+}
+}
